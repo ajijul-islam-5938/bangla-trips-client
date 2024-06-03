e83a1d7b041448e1113e97b1b8c59f3641b20d7f
shafiul-osd <mdshafiulislamsafi2506@gmail.com>
2024-06-03 16:58:56 +0600

    make admin functionality added

--- src/Pages/Dashboard/AdminDashboard/ManageUsers/ManageUsers.jsx
+++ src/Pages/Dashboard/AdminDashboard/ManageUsers/ManageUsers.jsx
@@ -3,18 +3,60 @@ import { useQuery } from "@tanstack/react-query";
 import useAxios from "../../../../Hooks/useAxios";
 import useAdmin from "../../../../Hooks/useAdmin";
 import { RiAdminLine } from "react-icons/ri";
+import Swal from "sweetalert2";
 const TABLE_HEAD = ["Name", "Job", "Employed", ""];
 
 const ManageUsers = () => {
   const { axiosSecure } = useAxios();
 
-  const { data: users } = useQuery({
+  const { data: users, refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
       return res.data;
     },
   });
+
+  const handleAdmin = (_id, user) => {
+    axiosSecure
+      .patch(`/user/admin/${_id}`)
+      .then(res => {
+        Swal.fire({
+          icon: "success",
+          title: "Success!!",
+          text: `${user.name} is now ADMIN`,
+        });
+        refetch();
+      })
+      .catch(err => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: err.message,
+        });
+      });
+  };
+
+  const handleGuide = (_id, user) => {
+    axiosSecure
+      .patch(`/user/guide/${_id}`)
+      .then(res => {
+        Swal.fire({
+          icon: "success",
+          title: "Success!!",
+          text: `${user.name} is now ADMIN`,
+        });
+        refetch();
+      })
+      .catch(err => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: err.message,
+        });
+      });
+  };
+
   return (
     <div>
       <h1 className="text-center font-semibold text-2xl my-16">Manage Users</h1>
@@ -68,7 +110,7 @@ const ManageUsers = () => {
                     {user.role === "admin" ? (
                       <Chip
                         variant="ghost"
-                        color="green"
+                        color="light-blue"
                         size="sm"
                         value="Admin"
                         className="text-center"
@@ -76,7 +118,7 @@ const ManageUsers = () => {
                     ) : user.role === "guide" ? (
                       <Chip
                         variant="ghost"
-                        color="green"
+                        color="light-green"
                         size="sm"
                         value="Guide"
                         className="text-center"
@@ -92,7 +134,7 @@ const ManageUsers = () => {
                     ) : (
                       <Chip
                         variant="ghost"
-                        color="green"
+                        color="blue-gray"
                         size="sm"
                         value="Tourist"
                         className="text-center"
@@ -106,10 +148,22 @@ const ManageUsers = () => {
                     color="blue-gray"
                     className="font-medium flex gap-2 items-center"
                   >
-                    <Button variant="outlined" size="small" color="orange">
+                    <Button
+                      onClick={() => handleGuide(user._id, user)}
+                      variant="outlined"
+                      size="sm"
+                      color="deep-orange"
+                      disabled={user.role === "requested" ? false : true}
+                    >
                       Make Guide
                     </Button>
-                    <Button variant="gradient" size="small" color="blue">
+                    <Button
+                      onClick={() => handleAdmin(user._id, user)}
+                      variant="gradient"
+                      size="sm"
+                      color="blue"
+                      disabled={user.role === "admin" ? true : false}
+                    >
                       make admin
                     </Button>
                   </Typography>
